20404ffa32106bd5d1d540da3afec50131ec8403
Ratanak123456 <ratanaksaren3@gmail.com>
2025-07-11 19:51:16 +0700

    inprove function sum

--- src/js/sum.js
+++ src/js/sum.js
@@ -1,9 +1,18 @@
+function sum() {
+  let num1 = parseInt(document.getElementById("number1").value);
+  let num2 = parseInt(document.getElementById("number2").value);
+  let result = num1 + num2;
 
-function sum () {
-
-    let a = parseInt(document.getElementById('number1').value);
-    let b = parseInt(document.getElementById('number2').value);
-
-    let result = a + b
-    document.getElementById('result').textContent = result; 
+  if (isNaN(num1) && isNaN(num2)) {
+    document.getElementById("result").textContent = "Please Input the Number";
+  } else if (Number(num1) && isNaN(num2)) {
+    document.getElementById("result").textContent =
+      "Please Input the Second Number";
+  } else if (isNaN(num1) && Number(num2)) {
+    document.getElementById("result").textContent =
+      "Please Input the First Number";
+  } else {
+    document.getElementById("result").textContent = result;
+  }
 }
+
